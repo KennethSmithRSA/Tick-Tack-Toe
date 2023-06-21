47d61a28022f9cc43f23ab849d1cf96a128296e8
KennethSmithRSA <firesmiths.kvs480@gmail.com>
2023-06-21 15:39:22 +0800

    Add logic to create new array based on in play gameboard, which is then test by the checkGameEndConditions to see whether game should carry on or end in win or tie (revise tie logic, user can click on cell multiple times increasing count).

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ const Player = (name, symbol) => {
     return {name, symbol}
 }
 
+let player1 = Player('player1', 'X');
+let player2 = Player('player2', 'O');
+
+let playerFlag = player2;
+
+let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
 const gameControl = (() => {
     const renderGameBoard = () => {
         const boardContainer = document.querySelector('.boardContainer');
@@ -24,23 +31,59 @@ const gameControl = (() => {
         }
     };
 
+    let count = 0;
+
     const ChangeSymbol = () => {
         let cells = document.querySelectorAll('.cell');
         cells.forEach((cell) => {
             cell.addEventListener('click', () => {
-                if (player === player2) {
-                    player = player1;
-                } else {
-                    player = player2;
-                }
-
                 if (cell.textContent === 'X' || cell.textContent === 'O') {
                     cell.textContent = cell.textContent;
                 } else {
-                    cell.textContent = player.symbol;
+                    if (playerFlag === player2) {
+                        playerFlag = player1;
+                    } else {
+                        playerFlag = player2;
+                    }
+                    cell.textContent = playerFlag.symbol;
                 }
+                arr.splice(cell.dataset.index, 1, cell.textContent);
+                cell.preventDefault();
+                count++;
+                console.log(arr);
+                checkGameEndConditions(arr)
             })
         });
+    };
+
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    const checkGameEndConditions = (arr) => {
+        let roundWin = false;
+        for (let i = 0; i <= 7; i++) {
+            const winCondition = winningConditions[i];
+            let a = arr[winCondition[0]];
+            let b = arr[winCondition[1]];
+            let c = arr[winCondition[2]];
+
+            if (a === b && b === c) {
+                console.log('Win');
+                break;
+            }
+
+            if (count === 9 && roundWin === false) {
+                console.log('tie');
+            }
+        }
     }
 
     return {
@@ -48,15 +91,6 @@ const gameControl = (() => {
         ChangeSymbol,
     }
 })();
-
-
-
-let player1 = Player('player1', 'X');
-let player2 = Player('player2', 'O');
-
-let player = player2;
-
-
 
 gameControl.renderGameBoard();
 gameControl.ChangeSymbol();
